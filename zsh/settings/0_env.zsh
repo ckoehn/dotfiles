5e627d863a8864793a4b974eff72494d9600aa98
Christian Köhn <christian@ckoehn.de>
2018-03-25 12:34:59 +0200

    Use ag for fzf

--- zsh/settings/0_env.zsh
+++ zsh/settings/0_env.zsh
@@ -6,4 +6,5 @@ export PATH="$HOME/bin:$HOME/.bin:$HOME/.local/bin:/snap/bin:$PATH"
 export NVM_DIR="${HOME}/.nvm"
 export NVM_LAZY_LOAD=true
 
+export FZF_DEFAULT_COMMAND='ag -g ""'
 export FZF_DEFAULT_OPTS='--reverse'
