f02d47be2a294c54d984be50407d9878a5332056
Christian Köhn <christian@ckoehn.de>
2018-10-06 15:50:43 +0200

    Add i3 configuration

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,4 +3,8 @@
 
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-polybar top &
+
+# https://github.com/jaagr/polybar/issues/763
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload top &
+done
