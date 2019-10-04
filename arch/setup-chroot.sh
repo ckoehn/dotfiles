d52a21b41294eb63147cd2acfa2c402b38dd413b
Christian Köhn <christian@ckoehn.de>
2019-10-04 18:10:49 +0200

    arch: Enable docker

--- arch/setup-chroot.sh
+++ arch/setup-chroot.sh
@@ -16,4 +16,4 @@ sed -i "s/^#\(autologin-user=\)$/\1${USERNAME}/" /etc/lightdm/lightdm.conf
 # Docker without sudo
 gpasswd -a ${USERNAME} docker
 
-systemctl enable NetworkManager lightdm systemd-timesyncd tlp
+systemctl enable NetworkManager lightdm systemd-timesyncd tlp docker
